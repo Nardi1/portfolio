807db6cab54fe0aa448dda75ac3da422d675e50d
nardinomook <thomasmuller2@gmail.com>
2017-05-12 18:06:48 +0200

    premier commit

---
+++ index.php
@@ -0,0 +1,27 @@
+
+<?php  
+
+require 'parts/functions.php';
+	$whitelist = ['home','creation', 'projet', 'contact'];
+
+	part("header");
+
+
+
+	if ($_GET['page'] == 'projet' && !isset($_GET['projet'])) {
+		part("404");
+	} else {
+
+	if (!isset($_GET['page'])) {
+		require 'parts/page/home.php';
+	} else {
+		$page = 'parts/page/' . $_GET['page'] . '.php';
+		if (file_exists($page) && in_array($_GET['page'], $whitelist)) {
+			require $page;
+		}	else {
+			part("404");
+		}
+	} }
+	part("footer");
+	?>
+
