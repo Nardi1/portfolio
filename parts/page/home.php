f04f573d7f14f88654dfa95babf7c36d1215e101
1r2e3d4a <MALAS@MacBook-Pro-de-Utilisateur.local>
2017-05-15 12:37:16 +0200

    homepage done

--- parts/page/home.php
+++ parts/page/home.php
@@ -2,7 +2,7 @@
         <div class="container">
             <div class="intro-text">
                 <div class="intro-lead-in">Bienvenue sur mon Portfolio</div>
-                <div class="intro-heading">Jixart</div>
+                <div class="intro-heading">Jix<span class="art">art</span></div>
                 <a href="index.php?page=creation" class="btn btn-primary">Voir les creations</a>
             </div>
         </div>
